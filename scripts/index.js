f3c1bce019630bf9a7e923ab8d5f93bb701fe74d
aviel drori <aviel.drori@mail.huji.ac.il>
2020-09-15 21:54:30 +0300

    a

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+
+
+
+(function(window, document, undefined)
+{
+  var image_interest = document.getElementById('interests-container');
+  image_interest.addEventListener('mouseover', imageInterestMouseOver, false);
+  image_interest.addEventListener('mouseout', imageInterestMouseOut, false);
+
+  var image_plane = document.getElementById('contact-container');
+  image_plane.addEventListener('mouseover', imagePlaneMouseOver, false);
+  image_plane.addEventListener('mouseout', imagePlaneMouseOut, false);
+ 
+})(window, window.document);
+
+
+function imageInterestMouseOver(event)
+{
+    event = event || window.event;
+
+    var image = document.getElementById('interests');
+    image.src = './resources/icons/interest_color.png'
+}
+
+function imageInterestMouseOut(event)
+{
+  event = event || window.event;
+
+  var image = document.getElementById('interests');
+  image.src = './resources/icons/interest.png'
+}
+
+
+function imagePlaneMouseOver(event)
+{
+    event = event || window.event;
+
+    var image = document.getElementById('plane');
+    image.src = './resources/icons/plane_move.png'
+}
+
+function imagePlaneMouseOut(event)
+{
+  event = event || window.event;
+
+  var image = document.getElementById('plane');
+  image.src = './resources/icons/plane.png'
+}
